fdfb0827d5120cf978ca2f4eb699a3a574aa30f5
lotusgraham <gwhitley99@gmail.com>
2016-04-19 15:48:54 +0000

    adds data object with part, api, and question

--- js/app.js
+++ js/app.js
@@ -1,17 +1,25 @@
 var thinktube = {
-    url: 'https://api.github.com/users/lotusgraham/repos',
-    // url: 'https://www.googleapis.com/youtube/v3/search',
+    // url: 'https://api.github.com/users/lotusgraham/repos',
+    url: 'https://www.googleapis.com/youtube/v3/search/',
     dataType: 'json',
+    data:{
+        part: 'snippet',
+        key: 'AIzaSyDtULH2PxGsNF3BAQnF8HfFqGQGsl7qv0Y',
+        q:  'batman'
+    },
+    
     success: function(data) {
+                console.log(data);
         var items = data.map(function(repo){
             return '<li>' + repo.name + '</li>';
         });
         $('#searchresults').append(items);
-        console.log(data);
     }
 };
 
 $.ajax(thinktube);
+
+//  https://www.youtube.com/results?search_query=funniest+youtube+videos&page=&utm_source=opensearch
 
 // $(function(data){
 //     $.getJSON('https://www.omdbapi.com/?s=Star%20Wars&r=json', function(){
