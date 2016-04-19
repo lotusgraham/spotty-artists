34363ce3d5c02912db48768007fbd7cc092f1e15
lotusgraham <gwhitley99@gmail.com>
2016-04-19 15:24:42 +0000

    adds function to display api data from github

--- js/app.js
+++ js/app.js
@@ -1,5 +1,17 @@
-$
+var thinktube = {
+    url: 'https://api.github.com/users/lotusgraham/repos',
+    // url: 'https://www.googleapis.com/youtube/v3/search',
+    dataType: 'json',
+    success: function(data) {
+        var items = data.map(function(repo){
+            return '<li>' + repo.name + '</li>';
+        });
+        $('#searchresults').append(items);
+        console.log(data);
+    }
+};
 
+$.ajax(thinktube);
 
 // $(function(data){
 //     $.getJSON('https://www.omdbapi.com/?s=Star%20Wars&r=json', function(){
