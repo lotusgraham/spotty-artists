04aef299fdc1fadfb19b83b65a8ae7daa0bc6d08
Scott Diallo <diallot@me.com>
2016-04-19 07:57:24 -0700

    wireframe built

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
-$(function(data){
-    $.getJSON('https://www.omdbapi.com/?s=Star%20Wars&r=json', function(){
-    console.log(data);
-  });
-});
+$
+
+
+// $(function(data){
+//     $.getJSON('https://www.omdbapi.com/?s=Star%20Wars&r=json', function(){
+//     console.log(data);
+//   });
+// });
+
+//AIzaSyDtULH2PxGsNF3BAQnF8HfFqGQGsl7qv0Y
