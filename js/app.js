c80f5872cc60d1d1fda82990044925e0b0fdf3c2
lotusgraham <gwhitley99@gmail.com>
2016-04-19 16:05:35 -0400

    youtube app working, and thumbnail links to videos clickable

--- js/app.js
+++ js/app.js
@@ -1,15 +1,11 @@
 
- // var userInput = $('#query').val();
-
-
 $('#btn').click(function(){
-    // thinktube.data.q = $('#query').val();
     $('#searchresults').empty();
     getYoutube($('#query').val());
 });
+
 function getYoutube(userInput){
     var thinktube = {
-        // url: 'https://api.github.com/users/lotusgraham/repos',
         url: 'https://www.googleapis.com/youtube/v3/search/',
         dataType: 'json',
         data:{
@@ -18,33 +14,15 @@ function getYoutube(userInput){
             q:  userInput //search input
         },
 
-        //build function where upon click of submit button, input value from user is logged into a variable
-        //take said variable which containts the input value and stick it in our data object
-
-        success: function(data) {
-                    console.log(data.items);
+            success: function(data) {
+                    console.log(data);
                 for (var i = 0; i < data.items.length; i++){
-                    console.log(data.items[i].snippet.title);
+                    var video = data.items[i];
+                    console.log("https://www.youtube.com/watch?v=" + video.id.videoId);
+                    $('#searchresults').append("<li> <b>" + video.snippet.title + "</b> </li>");
+                    $('#searchresults').append("<a href='https://www.youtube.com/watch?v='" + video.id.videoId + "'><img src='" + video.snippet.thumbnails.default.url + "'></a>");
                 }
-            //  var items = data.items.map(function(video){
-            //     //  console.log(video.snippet.title);
-            //      console.log(video.snippet.thumbnails.default.url);
-            //      $('#searchresults').append("<li> <b>" + video.snippet.title + "</b> </li>");
-            //      $('#searchresults').append("<img src='" + video.snippet.thumbnails.default.url + "'>");
-            //  });
-            
         }
     };
     $.ajax(thinktube);
 }
-// "<img src="+items[0].snipppet.thumbnails.medium.url+">;"
-
-//  https://www.youtube.com/results?search_query=funniest+youtube+videos&page=&utm_source=opensearch
-
-// $(function(data){
-//     $.getJSON('https://www.omdbapi.com/?s=Star%20Wars&r=json', function(){
-//     console.log(data);
-//   });
-// });
-
-//AIzaSyDtULH2PxGsNF3BAQnF8HfFqGQGsl7qv0Y
