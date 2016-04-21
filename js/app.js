2a1dbdb399ea82bb69a273b74442d21b76b0f5a2
lotusgraham <gwhitley99@gmail.com>
2016-04-21 19:33:10 +0000

    adds get request for search by artist, displays artist, and adds get request for related artists

--- js/app.js
+++ js/app.js
@@ -1,28 +1,72 @@
-
-$('#btn').click(function(){
-    $('#searchresults').empty();
-    getYoutube($('#query').val());
+$('#btn').click(function() {
+    $('#listResults').empty();
+    searchByArtist($('#query').val());
 });
 
-function getYoutube(userInput){
-    var thinktube = {
-        url: 'https://www.googleapis.com/youtube/v3/search/',
-        dataType: 'json',
-        data:{
-            part: 'snippet',
-            key: 'AIzaSyDtULH2PxGsNF3BAQnF8HfFqGQGsl7qv0Y',
-            q:  userInput //search input
-        },
+var displayTopArtist  = function(imgUrl) {
+    console.log('xxxxxxxx');
+    $('#listResults').append("<img class='main-image' src='" + imgUrl + "'/>");
+    
+};
 
-            success: function(data) {
-                    console.log(data);
-                for (var i = 0; i < data.items.length; i++){
-                    var video = data.items[i];
-                    console.log("https://www.youtube.com/watch?v=" + video.id.videoId);
-                    $('#searchresults').append("<li> <b>" + video.snippet.title + "</b> </li>");
-                    $('#searchresults').append("<a href='https://www.youtube.com/watch?v='" + video.id.videoId + "'><img src='" + video.snippet.thumbnails.default.url + "'></a>");
-                }
-        }
+var searchByArtist = function(userInput) {
+
+    var request = {
+        q: userInput,
+        type: 'artist'
     };
-    $.ajax(thinktube);
-}
+
+    $.ajax({
+            url: "https://api.spotify.com/v1/search",
+            data: request,
+            dataType: "json", //use jsonp to avoid cross origin issues
+            type: "GET",
+        })
+        .done(function(result) { //this waits for the ajax to return with a succesful promise object
+            console.log(result);
+            var searchResults = "We found " + result.artists.items.length + " results for " + request.q;
+            var mainArtistImageUrl = result.artists.items[0].images[1].url;
+            
+            console.log(mainArtistImageUrl);
+            
+            console.log('tried to displayTopArtist()');
+             displayTopArtist(mainArtistImageUrl);
+             
+             findRelatedArtists(result.artists.items[0].id);
+            			
+        })
+    .fail(function(jqXHR, error) { //this waits for the ajax to return with an error promise object
+        console.log(error);
+        $('#searchresults').text("An error occurred or no results were found");
+    });
+};
+
+
+var findRelatedArtists = function(artistId) {
+
+    $.ajax({
+            url: "https://api.spotify.com/v1/artists/" + artistId + "/related-artists",
+            // data: request,
+            dataType: "json", //use jsonp to avoid cross origin issues
+            type: "GET",
+        })
+        .done(function(result) { //this waits for the ajax to return with a succesful promise object
+            console.log(result);
+            			
+        })
+
+    .fail(function(jqXHR, error) { //this waits for the ajax to return with an error promise object
+        console.log(error);
+    });
+};
+
+
+
+
+
+
+
+// $.each(result.artists.items, function(i, item) {
+//             				var artists = result.artists.items[i].name;
+//             				$('#searchresults').append("<li> <b>" + artists + "</b> </li>");
+//             			});
---
+++ js/app.saved-working.js
@@ -0,0 +1,47 @@
+
+$('#btn').click(function(){
+    $('#searchresults').empty();
+    getYoutube($('#query').val());
+});
+
+
+
+
+function getYoutube(userInput){
+    var thinktube = {
+        url: 'https://www.googleapis.com/youtube/v3/search/',
+        dataType: 'json',
+        data:{
+            part: 'snippet',
+            key: 'AIzaSyDtULH2PxGsNF3BAQnF8HfFqGQGsl7qv0Y',
+            q:  userInput //search input
+        },
+
+            success: function(data) {
+                    console.log(data);
+                for (var i = 0; i < data.items.length; i++){
+                    var video = data.items[i];
+                    console.log("https://www.youtube.com/watch?v=" + video.id.videoId);
+                    $('#searchresults').append("<li> <b>" + video.snippet.title + "</b> </li>");
+                    $('#searchresults').append("<a href='https://www.youtube.com/watch?v='" + video.id.videoId + "'><img src='" + video.snippet.thumbnails.default.url + "'></a>");
+                }
+        }
+    };
+    $.ajax(thinktube);
+}
+
+
+// $(function() {
+  
+
+
+// $('div.class').hover(function() {
+//     $(this).css('backround-color', 'blue')
+// }, function() {
+//     $(this).css('backround-color', 'crimson')
+// }), 
+     
+    
+// });
+
+
