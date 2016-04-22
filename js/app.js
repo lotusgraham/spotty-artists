bebb62cd72854a4641a6e789f7b4510f223a6767
Kyle <kyleludlow@gmail.com>
2016-04-21 20:31:00 -0700

    minor reformatting

--- js/app.js
+++ js/app.js
@@ -1,23 +1,24 @@
-$('#artistSearchForm').on('submit', function(e) {
+$('#artistSearchForm').on('submit', function (e) {
     e.preventDefault();
     $('#listResults').empty();
     searchByArtist($('#query').val());
 });
 
-var displayTopArtist  = function(imgUrl) {
+var displayTopArtist = function (imgUrl) {
     console.log('xxxxxxxx');
     $('.mainArtist').append("<img class='main-image' src='" + imgUrl + "'/>");
-    
 };
-var displayRelatedArtist  = function(artist1, artist2, artist3) {
+
+var displayRelatedArtist = function (artist1, artist2, artist3) {
     console.log('xxxxxxxx');
     $('.relatedArtist1').append("<img class='first-rel' src='" + artist1.image + "'/><p>" + artist1.name + "</p>");
     $('.relatedArtist2').append("<img class='second-rel' src='" + artist2.image + "'/><p>" + artist2.name + "</p>");
     $('.relatedArtist3').append("<img class='third-rel' src='" + artist3.image + "'/><p>" + artist3.name + "</p>");
-
-    
 };
-var searchByArtist = function(userInput) {
+
+
+//API call to search by artist
+var searchByArtist = function (userInput) {
 
     var request = {
         q: userInput,
@@ -27,66 +28,52 @@ var searchByArtist = function(userInput) {
     $.ajax({
             url: "https://api.spotify.com/v1/search",
             data: request,
-            dataType: "json", //use jsonp to avoid cross origin issues
-            type: "GET",
+            dataType: "json",
+            type: "GET"
         })
-        .done(function(result) { //this waits for the ajax to return with a succesful promise object
+        .done(function (result) {
             console.log(result);
-            var searchResults = "We found " + result.artists.items.length + " results for " + request.q;
+
             var mainArtistImageUrl = result.artists.items[0].images[1].url;
-            
-            console.log(mainArtistImageUrl);
-            
-            console.log('tried to displayTopArtist()');
-             displayTopArtist(mainArtistImageUrl);
-             
-             findRelatedArtists(result.artists.items[0].id);
-            			
+
+            displayTopArtist(mainArtistImageUrl);
+
         })
-    .fail(function(jqXHR, error) { //this waits for the ajax to return with an error promise object
-        console.log(error);
-        $('#searchresults').text("An error occurred or no results were found");
-    });
+        .done(function(result) {
+            findRelatedArtists(result.artists.items[0].id);
+        })
+        .fail(function (jqXHR, error) {
+            console.log(error);
+            $('#searchresults').text("An error occurred or no results were found");
+        });
 };
 
-
-var findRelatedArtists = function(artistId) {
+//API call to get related artist info based on searched artist ID
+var findRelatedArtists = function (artistId) {
 
     $.ajax({
             url: "https://api.spotify.com/v1/artists/" + artistId + "/related-artists",
-            // data: request,
-            dataType: "json", //use jsonp to avoid cross origin issues
-            type: "GET",
+            dataType: "json",
+            type: "GET"
         })
-        .done(function(result) { //this waits for the ajax to return with a succesful promise object
+        .done(function (result) {
             console.log(result);
-           var relatedArtist1 = {
-               image: result.artists[0].images[2].url,
-               name: result.artists[0].name
-               },
-               relatedArtist2 = {
-                image: result.artists[1].images[2].url,
-                name: result.artists[1].name
-               },
-               relatedArtist3 = {
-                   image: result.artists[2].images[2].url,
+            var relatedArtist1 = {
+                    image: result.artists[0].images[2].url,
+                    name: result.artists[0].name
+                },
+                relatedArtist2 = {
+                    image: result.artists[1].images[2].url,
+                    name: result.artists[1].name
+                },
+                relatedArtist3 = {
+                    image: result.artists[2].images[2].url,
                     name: result.artists[2].name
-                    };
+                };
 
-           displayRelatedArtist(relatedArtist1, relatedArtist2, relatedArtist3);
-           })
-    .fail(function(jqXHR, error) { //this waits for the ajax to return with an error promise object
-        console.log(error);
-    });
+            displayRelatedArtist(relatedArtist1, relatedArtist2, relatedArtist3);
+        })
+        .fail(function (jqXHR, error) {
+            console.log(error);
+        });
 };
-
-
-
-
-
-
-
-// $.each(result.artists.items, function(i, item) {
-//             				var artists = result.artists.items[i].name;
-//             				$('#searchresults').append("<li> <b>" + artists + "</b> </li>");
-//             			});
